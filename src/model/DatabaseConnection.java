70b1790a496abcc6d4598cf66568f2987c495035
홍유진 <dbwlsyyy@naver.com>
2024-11-07 18:56:56 +0900

    test

--- src/model/DatabaseConnection.java
+++ src/model/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USER = "root";
-    static final String PASSWORD = System.getenv("DB_PASSWORD"); // 환경변수 사용
-    //private static final String PASSWORD = ""; // 환경변수 사용 x
+    //static final String PASSWORD = System.getenv("DB_PASSWORD"); // 환경변수 사용
+    private static final String PASSWORD = "1234"; // 환경변수 사용 x
 
     public static Connection getConnection() {
         try {
